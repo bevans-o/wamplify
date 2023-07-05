ebf04477746fad1fe1614247afc0cab115499628
bevans-o <81346899+bevans-o@users.noreply.github.com>
2023-07-05 23:19:47 +1000

    subject entry props

--- wamplify/src/app/components/Sidebar/SubjectEntry/SubjectEntry.tsx
+++ wamplify/src/app/components/Sidebar/SubjectEntry/SubjectEntry.tsx
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import entry from './entry.module.css'
 import Divider from '../../misc/Divider'
 
-function SubjectEntry() {
+function SubjectEntry({_code, _valid, _assessmentItems} : any) {
+  const [subjectCode, setSubjectCode] = useState(_code);
+  const [valid, setValid] = useState(_valid);
+  const [assessmentItems, setAssessmentItems] = useState(_assessmentItems);
+
   return (
     <div className={entry.container}>
       <div className="fc">
-        <label className={entry.subject_title}>Designing Novel Interactions</label>
-        <input></input>
+        <label className={entry.subject_title}>...Subject Name</label>
+        <input defaultValue={subjectCode}/>
       </div>
 
-      <Divider/>
+      {!valid && subjectCode && 
+        <p>Please enter a valid subject code!</p>
+      }
 
-      <div>
+      {valid && 
+        <>
+          <Divider/>
+          <div>
+            {assessmentItems?.map((item : any, index: number) => {
+              return (
+                <div key={index}>
+                  <p>{item.title}</p>
+                  <input defaultValue={item.weight}/>
+                </div>
+              )
+            })}
+          </div>
+        </>
+      }
 
-      </div>
+      
+
+      
     </div>
   )
 }
--- wamplify/src/app/page.tsx
+++ wamplify/src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import wamplify from './page.module.css'
 import Infobar from './components/Infobar/Infobar'
 import Sidebar from './components/Sidebar/Sidebar'
@@ -9,10 +11,40 @@ export default function Home() {
   return (
     <main className={wamplify.main}>
       <Sidebar>
+        <SubjectEntry _code={"INFO30005"} _valid={true} _assessmentItems={
+          [
+            {
+              title: "Assignment 1",
+              weight: 30
+            },
+            {
+              title: "Assignment 2",
+              weight: 70
+            }
+          ]
+        }/>
+        <SubjectEntry _code={"lalalala"} _valid={false} _assessmentItems={[]}/>
         <SubjectEntry/>
-        <SubjectEntry/>
-        <SubjectEntry/>
-        <SubjectEntry/>
+        <SubjectEntry _code={"INFO30005"} _valid={true} _assessmentItems={
+          [
+            {
+              title: "Assignment 1",
+              weight: 30
+            },
+            {
+              title: "Assignment 2",
+              weight: 70
+            },
+            {
+              title: "Assignment 3",
+              weight: 70
+            },
+            {
+              title: "Assignment 4 - an assignment with an extremely long name that really should be much shorter but the subject coordinator struggles with conciseness",
+              weight: 70
+            }
+          ]
+        }/>
       </Sidebar>
 
       <ContentArea>
