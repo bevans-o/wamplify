990df3cc81ed9894be5422281e1a3bdf308ff391
risamp <risapais@gmail.com>
2023-07-29 18:19:03 +1000

    make subjects searchable

--- wamplify/next.config.cjs
+++ wamplify/next.config.cjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nodeExternals = require('webpack-node-externals');
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = {
+  ...nextConfig,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals = [nodeExternals()];
+    }
+
+    return config;
+  },
+};
