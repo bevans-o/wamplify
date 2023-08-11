c0b534c21fa7d9734121cc34aef6bc8731426f06
bevans-o <81346899+bevans-o@users.noreply.github.com>
2023-08-11 14:15:56 +1000

    fixed swiper layout

--- wamplify/src/app/components/PanelSlider/PanelSlider.tsx
+++ wamplify/src/app/components/PanelSlider/PanelSlider.tsx
@@ -4,6 +4,7 @@ import slider from './slider.module.css'
 import Wamplifier from '../Wamplifier/Wamplifier'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { FreeMode } from 'swiper/modules';
+import 'swiper/css';
 import Button from '../Button/Button';
 import AddIcon from '@mui/icons-material/Add';
 import { Subject } from '@/app/types/types';
@@ -30,6 +31,7 @@ function PanelSlider() {
 
   return (
     <div className={slider.container}>
+
         <Swiper
           slidesPerView={'auto'}
           spaceBetween={0}
@@ -38,10 +40,9 @@ function PanelSlider() {
           freeMode={true}
           preventClicks={false}
           modules={[FreeMode]}
-          className={slider.swiper}
         >
           {subjects.map((subject: Subject, index: number) => 
-            <SwiperSlide>
+            <SwiperSlide key={index}>
                 <Wamplifier subject={subject}/>
             </SwiperSlide>
           )}
@@ -56,6 +57,7 @@ function PanelSlider() {
           </SwiperSlide>
           
         </Swiper>
+        
     </div>
   )
 }
