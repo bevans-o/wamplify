cbf6aec9e79652b3f67d1c2870638cfb7886661b
risamp <risapais@gmail.com>
2023-08-14 14:11:15 +1000

    assessment result input validation & update predicted average

--- wamplify/src/app/components/Wamplifier/Assessment/Assessment.tsx
+++ wamplify/src/app/components/Wamplifier/Assessment/Assessment.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Assessment } from '@/app/types/types';
 import assessmentItem from './assessment.module.css'
+import getAssessmentScore from '@/app/utils/scripts/getAssessmentScore';
 
 interface AssessmentProps {
   assessment: Assessment;
@@ -16,20 +17,67 @@ function Assessment({assessment, highlighted, onChange} : AssessmentProps) {
     //const score : HTMLElement | null = document.querySelector(`input`)
   })
 
+  const isValid = (score : string) => {
+    //check numbera/numberb is a valid format
+    let slashSplit = score.split('/');
+    if (slashSplit.length == 2) {
+      let a = parseInt(slashSplit[0]);
+      let b = parseInt(slashSplit[1]);
+      if (isNaN(a) || isNaN(b)) {
+        return false
+      }
+      if (a > b) {
+        return false
+      }
+      return true
+    } 
+
+    // check percentage is a valid format
+    let percentageSplit = score.split('%');
+    if (percentageSplit.length > 2) {
+      return false;
+    }
+    if (percentageSplit.length == 2) {
+      if (percentageSplit[1] != '') {
+        return false
+      }
+      let a = parseInt(percentageSplit[0])
+      if (isNaN(a)) {
+        return false
+      }
+      if (a > 100 || a < 0) {
+        return false
+      }
+      return true;
+    }
+
+
+    //check integer is valid
+    let a = parseInt(score);
+    if (isNaN(a) || a > 100 || a < 0) {
+      return false;
+    }
+    return true;
+  }
+
   const onScoreChange = (event: React.SyntheticEvent) => {
-    console.log("calling onScoreChange");
     let scoreInput = event.target as HTMLInputElement;
+
+    //resets subject if input is nothing
     if (scoreInput.value.length == 0){
-      setScoreInput("");
       assessment.completed = false;
-      onChange();
+      return;
     }
-    else {
-      setScoreInput(scoreInput.value);
-      assessment.score = parseInt(scoreInput.value);
+
+    //otherwise sets new input field value and assessment score
+    if (isValid(score)) {
+      let subjectScore = getAssessmentScore(scoreInput.value);
+      setScoreInput(subjectScore.toString() + "%")
+      assessment.score = subjectScore
       assessment.completed = true;
-      onChange();
     }
+
+    onChange();
   }
 
 
@@ -41,11 +89,11 @@ function Assessment({assessment, highlighted, onChange} : AssessmentProps) {
           {assessment.title}
         </span>
         <span className={assessmentItem.description}>
-          TODO: descriptions
+          {assessment.weight + "%"}
         </span>
       </div>
 
-      <input value={score} onChange={(e) => onScoreChange(e)}/>
+      <input value={score} onChange={(e) => setScoreInput(e.target.value)}onBlur={(e) => onScoreChange(e)} className={isValid(score) ? "" : assessmentItem.invalid}/>
 
       <div className={assessmentItem.wire + " " + (highlighted ? assessmentItem.wireHighlight : "")}></div>
       
