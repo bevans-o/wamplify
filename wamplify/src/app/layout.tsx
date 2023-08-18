99b8bae8d9b252eacc88077768f3b0fbc46eb523
bevans-o <81346899+bevans-o@users.noreply.github.com>
2023-08-18 17:20:41 +1000

    analytics

--- wamplify/src/app/layout.tsx
+++ wamplify/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Wamplify',
@@ -28,7 +29,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics/>
+      </body>
     </html>
   )
 }
