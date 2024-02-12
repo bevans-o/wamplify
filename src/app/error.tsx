9e8a04d22c13f8f5a6bffb436c89addec0d039ae
bevans <81346899+bevans-o@users.noreply.github.com>
2024-02-13 10:49:44 +1100

    add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import page from "./page.module.css";
+
+function Error() {
+  return (
+    <div className={page.error}>
+      <h2>Whoops! Something went wrong.</h2>
+      <p>
+        Try <a onClick={() => window.location.reload()}>refreshing the page.</a>
+      </p>
+    </div>
+  );
+}
+
+export default Error;
